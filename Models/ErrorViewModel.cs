d1b56333cc3c54eed843c38b8ed36327b353e13c
cnfrank <9570075@qq.com>
2024-02-25 17:01:14 +1300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FPW8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
